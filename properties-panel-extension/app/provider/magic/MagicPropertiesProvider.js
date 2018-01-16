a60b4b7f33e90b99a8a2336c2a1a05384ad8f3e3
vyomkesh <vyomkeshjha@yahoo.com>
2018-01-16 23:11:39 +0530

    Initial commit

---
+++ properties-panel-extension/app/provider/magic/MagicPropertiesProvider.js
@@ -0,0 +1,104 @@
+'use strict';
+
+
+var inherits = require('inherits');
+
+var PropertiesActivator = require('bpmn-js-properties-panel/lib/PropertiesActivator');
+
+// Require all properties you need from existing providers.
+// In this case all available bpmn relevant properties without camunda extensions.
+var processProps = require('bpmn-js-properties-panel/lib/provider/bpmn/parts/ProcessProps'),
+    eventProps = require('bpmn-js-properties-panel/lib/provider/bpmn/parts/EventProps'),
+    linkProps = require('bpmn-js-properties-panel/lib/provider/bpmn/parts/LinkProps'),
+    documentationProps = require('bpmn-js-properties-panel/lib/provider/bpmn/parts/DocumentationProps'),
+    idProps = require('bpmn-js-properties-panel/lib/provider/bpmn/parts/IdProps'),
+    nameProps = require('bpmn-js-properties-panel/lib/provider/bpmn/parts/NameProps');
+
+
+// Require your custom property entries.
+var spellProps = require('./parts/SpellProps');
+
+// The general tab contains all bpmn relevant properties.
+// The properties are organized in groups.
+function createGeneralTabGroups(element, bpmnFactory, elementRegistry, translate) {
+
+  var generalGroup = {
+    id: 'general',
+    label: 'General',
+    entries: []
+  };
+  idProps(generalGroup, element, translate);
+  nameProps(generalGroup, element, translate);
+  processProps(generalGroup, element, translate);
+
+  var detailsGroup = {
+    id: 'details',
+    label: 'Details',
+    entries: []
+  };
+  linkProps(detailsGroup, element, translate);
+  eventProps(detailsGroup, element, bpmnFactory, elementRegistry, translate);
+
+  var documentationGroup = {
+    id: 'documentation',
+    label: 'Documentation',
+    entries: []
+  };
+
+  documentationProps(documentationGroup, element, bpmnFactory, translate);
+
+  return[
+    generalGroup,
+    detailsGroup,
+    documentationGroup
+  ];
+}
+
+// Create the custom magic tab
+function createMagicTabGroups(element, elementRegistry) {
+
+  // Create a group called "Black Magic".
+  var blackMagicGroup = {
+    id: 'black-magic',
+    label: 'Black Magic',
+    entries: []
+  };
+
+  // Add the spell props to the black magic group.
+  spellProps(blackMagicGroup, element);
+
+  return [
+    blackMagicGroup
+  ];
+}
+
+function MagicPropertiesProvider(eventBus, bpmnFactory, elementRegistry, translate) {
+
+  PropertiesActivator.call(this, eventBus);
+
+  this.getTabs = function(element) {
+
+    var generalTab = {
+      id: 'general',
+      label: 'General',
+      groups: createGeneralTabGroups(element, bpmnFactory, elementRegistry, translate)
+    };
+
+    // The "magic" tab
+    var magicTab = {
+      id: 'magic',
+      label: 'Magic',
+      groups: createMagicTabGroups(element, elementRegistry)
+    };
+
+    // Show general + "magic" tab
+    return [
+      generalTab,
+      magicTab
+    ];
+  };
+}
+
+inherits(MagicPropertiesProvider, PropertiesActivator);
+
+module.exports = MagicPropertiesProvider;
